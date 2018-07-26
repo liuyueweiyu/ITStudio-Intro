5c4ddf508d28eead86135f357682ef3dc7270e09
guxiaowu <337118043@qq.com>
2018-07-26 17:04:38 +0800

    update

---
+++ src/component/Department/Department.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Department.scss";
+
+class Department extends React.Component{
+    render(){
+        return (
+            <div className="department">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+export default Department;
