9e3e39d2d825073950f1e4e4336097fc2f25d737
guxiaowu <337118043@qq.com>
2018-07-26 16:53:42 +0800

    first commit

---
+++ src/component/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Logo.scss";
+
+class Logo extends React.Component{
+    render(){
+        return(
+            <div className="logo">
+                <img src={require("./Logo.png")}/>
+            </div>
+        )
+    }
+}
+
+export default Logo;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import "./component/reset.css";
+import Test from "./component/Logo/Logo"
+// import Test from "./component/Department/Department";
+
+
+render(
+    <Test />,
+    document.getElementById("root")
+);
