034721223bb0ab30a1493fd21bae1086fa314216
guxiaowu <337118043@qq.com>
2018-08-05 15:19:26 +0800

    update 呜呜呜第二段后半段重写了QUQ

--- src/component/Atom/Atom.js
+++ src/component/Atom/Atom.js
@@ -1,26 +1,30 @@
 import React from "react";
 import "./Atom.scss";
 import ahead from "./ahead.js";
+import Selectbox from "../Selectbox/Selectbox";
 
 class Atom extends React.Component{
     
     constructor(props){
         super(props);
         this.state = {
-            first:false,
+            first:true,
             second:false,
-            small:false
+            third:false,
+            // small:false
         }
         setTimeout(() => {
             this.setState({
+                first:false,
                 second:true
-            })
+            });
+            setTimeout(() => {
+                this.setState({
+                    second: false,
+                    third: true
+                });
+            }, 7900);
         }, 7200);
-        setTimeout(() => {
-            this.setState({
-                small: true
-            })
-        }, 7700);
     }
 
     componentDidMount(){
@@ -32,19 +36,17 @@ class Atom extends React.Component{
             <div className="atom">
                 <canvas id="canvas"></canvas>
                 <div className="outer">
-                    <div className="first"></div>
-                    {this.state.second && (
-                        <div className="second"></div>                    
+                    {this.state.first &&(
+                        <div className="first"></div>
                     )}
-                    {this.state.small &&
-                        <div className="small">
-                            <svg width="110px" height="110px">
-                                <circle cx="55" cy="55" r="50" stroke-width="8.8" fill="none" className="circleone"></circle>
-                                <circle cx="55" cy="55" r="50" stroke-width="10" fill="none" class="circlesvg"></circle>
-                            </svg>
-                            <div className="smallball"></div>
-                        </div>
-                    }
+                    {this.state.second && (
+                        <div className="second">
+                            <Selectbox />
+                        </div>                    
+                    )}
+                    {this.state.third&&(
+                        <div className="third"></div>
+                    )}
                 </div>
                 <h1>人们总是独自前进</h1>
                 <h2>却时常忘记选择</h2>
@@ -59,9 +61,6 @@ export default Atom;
  ** 1. 粒子动画准备时间 1.7s ---------------------------1.7s-----19.5s
  ** 2. 第一个球出现 0.5---------------------------------2.2s-----20s
  ** 3. 第一个球运动 5s/ 人们总是独自前进 动画闪现5s-------7.2s-----25s
- ** 4. 两个小球从中间往两边移动 0.5s --------------------7.7s-------26s
- ** 5. 中间小球开始出现 0.3s ---------------------------8s -------27.3s
- ** 6. 中间小球两边旋转 5.4s ---------------------------13.4s
- ** 7. 中间的环闭紧 -----------------------------------14.751s
-
+ ** 4. Selectbox 7.9s ---------------------------------17.1s----32,1s
+ ** 5. 变大 1s ----------------------------------------18.1s---- 33.1s
  */
--- src/component/Selectbox/Selectbox.js
+++ src/component/Selectbox/Selectbox.js
@@ -5,9 +5,15 @@ class Selectbox extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            fisrt:true
+            fisrt:true,
+            second:false
         }
-
+        setTimeout(() => {
+            this.setState({
+                fisrt: false,
+                second: true
+            })
+        }, 300);
 
     }
 
@@ -16,11 +22,19 @@ class Selectbox extends React.Component{
             <div className="selectbox">
                 {this.state.fisrt &&
                     <React.Fragment>
-                        <div className="first"></div>
-                        <span className="line"></span>
-                        <span className="line"></span>
-                        <span className="line"></span>
-                        <span className="line"></span>
+                        <div className="selectbox-first"></div>
+                        <span className="selectbox-line"></span>
+                        <span className="selectbox-line"></span>
+                        <span className="selectbox-line"></span>
+                        <span className="selectbox-line"></span>
+                    </React.Fragment>
+                }
+                {this.state.second &&
+                    <React.Fragment>
+                        <div className="selectbox-block"></div>
+                        <div className="selectbox-block"></div>
+                        <div className="selectbox-block"></div>
+                        <div className="selectbox-block"></div>
                     </React.Fragment>
                 }
             </div>
